3a56fcfe292a1f716a9d0c3f31680e73a905b41c
Yahya Alshwaily <yahya_alshwaily@github.com>
2020-07-26 13:20:23 +0300

    basketball stat tool v1.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Basketball Stat Tool
- 
+**Basket Ball Stat Tool**
+
+In this program, basic data is pulled from the 'constants' file. This data contains information about a 
+number of players.
+
+Tasks of the program:
+
+1) Clean Data about the players
+
+- Clean the data about the player's height, by isolating the height string into an integer type
+- Convert the experience string value into a boolean value
+- Clean and organize the guardians values into a list of string(s) instead of a single string with the word 'and' in it
+
+2) Assign the players into 3 teams
+
+- Each team has to have the same number of players
+- The team has to have the same amount of experienced players as inexperienced players
+- Each team has to get a unique set of players
+
+3) Data display and user prompt
+
+- The user has to input the correct choice from the mainmenu and the teams menu
+- The user's input is then used to call the team's data displaying all the information regarding the team
+- In addition the player's average height is displayed
---
+++ app.py
@@ -0,0 +1,141 @@
+import constants
+import copy
+import sys
+
+teams_main = copy.deepcopy(constants.TEAMS)
+players_main = copy.deepcopy(constants.PLAYERS)
+
+panthers = teams_main [0] = []
+bandits = teams_main [1] = []
+warriors = teams_main [2] = []
+
+
+def clean_data_and_balance_teams():
+
+	panthers_xp = 0
+	panthers_inxp = 0
+	bandits_xp = 0
+	bandits_inxp = 0
+	warriors_xp = 0
+	warriors_inxp = 0
+	
+	for element in players_main:
+			
+		element['height'] = element['height'][0:2]
+		element['height'] = int(element['height'])
+		
+		if element['experience'] == 'NO':
+			element['experience'] = False
+			
+		elif element['experience'] == 'YES':							  
+			element['experience'] = True
+			
+		element['guardians'] = element['guardians'].split(' and ')
+	
+	for element in players_main:
+		element['guardians'] = ', '.join(element['guardians'])
+	
+	for player in players_main:
+			
+		if player['experience'] == False and panthers_inxp < 3:
+			panthers.append(player)
+			panthers_inxp += 1
+			
+		elif player['experience'] == True and panthers_xp < 3:
+			panthers.append(player)
+			panthers_xp += 1
+		elif player['experience'] == False and bandits_inxp < 3:
+			bandits.append(player)
+			bandits_inxp += 1
+		elif player['experience'] == True and bandits_xp < 3:
+			bandits.append(player)
+			bandits_xp += 1
+		elif player['experience'] == False and warriors_inxp < 3:
+			warriors.append(player)
+			warriors_inxp += 1
+		elif player['experience'] == True and warriors_xp < 3:
+			warriors.append(player)
+			warriors_xp += 1
+
+			
+def get_choice():
+	
+	print("""
+BASKETBALL TEAM STATS TOOL
+
+---- MENU----
+
+Here are your choices:
+
+ 1) Display Team Stats
+ 2) Quit
+  """)
+	
+	menu_choice = str(input("Enter an option > "))
+	while menu_choice != "1" and menu_choice != "2":
+		menu_choice = str(input("Please Enter an option from the choices > "))
+		
+		
+	if menu_choice == "2":
+			sys.exit()
+	
+	elif menu_choice == "1":
+		print("""
+1) Panthers
+2) Bandits
+3) Warriors
+	""")
+	
+	team_choice = input("Enter an option > ")
+	while team_choice != "1" and team_choice != "2" and team_choice != "3":
+		team_choice = input("Please Enter an option from the choices > ")
+		
+	return int(team_choice)
+
+
+def get_team(team_choice):
+	
+	team_heights = []
+	for element in teams_main[team_choice - 1]:
+		team_heights.append(element['height'])
+	
+	team_avg_height = float(sum(team_heights)/len(teams_main[team_choice - 1]))
+	
+	team_names = ['Panthers', 'Bandits', 'Warriors']
+	
+	team_name = f"\nTeam: {team_names[team_choice-1]} Stats\n"
+	
+	print(team_name+ "-" * len(team_name))
+	
+	print(f"Total Players: {len(teams_main[team_choice - 1])}")
+	
+	experienced_placeholder = []
+	for element in teams_main[team_choice - 1]:
+		experienced_placeholder.append(element['experience'])
+		
+	inexperienced_placeholder = len(teams_main[team_choice - 1]) - sum(experienced_placeholder)
+	
+	print(f"Total experienced: {sum(experienced_placeholder)}")
+	print(f"Total inexperienced: {inexperienced_placeholder}")
+	print(f"Average height: {team_avg_height:.2f}")
+	
+	store_names = []
+	for element in teams_main [team_choice - 1]:
+		store_names.append(element['name'])
+	
+	print("\nPlayers on Team:\n"," ", ", ".join(store_names))
+	
+	store_guardians = []
+	for element in teams_main [team_choice - 1]:
+		store_guardians.append(element['guardians'])
+
+	print("\nGuardians:\n"," ", ", ".join(store_guardians))	
+
+if __name__ == "__main__":
+	clean_data_and_balance_teams()
+	
+	ENTER = ''
+	while ENTER == '':
+		get_team(get_choice())
+		ENTER = input("\nPress ENTER to continue...")
+	
